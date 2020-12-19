1661ebea90596758ed41f4b80a01650f483f48a1
Boonie Attawut <puifai.ba@gmail.com>
2020-12-19 05:11:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = 1000
+  min_value = nil
   name_hash.each do |name, value|
     if value < min_value do
       min_value = value
