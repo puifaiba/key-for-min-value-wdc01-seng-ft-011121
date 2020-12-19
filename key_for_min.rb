a7ea529ea243076ee3823d03ad14cb9895e330da
Boonie Attawut <puifai.ba@gmail.com>
2020-12-19 05:26:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = Infinity
+  min_value = Float::INFINITY
   name_hash.each do |name, value|
     if value < min_value
       min_value = value
