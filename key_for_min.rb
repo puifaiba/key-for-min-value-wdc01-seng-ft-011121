3bf417ae7826cda191c8fa4071b48da4f97f6821
Boonie Attawut <puifai.ba@gmail.com>
2020-12-19 05:22:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,8 @@
 def key_for_min_value(name_hash)
   min_value = 1000
   name_hash.each do |name, value|
-    if value < min_value do
+    if value < min_value 
       min_value = value
     end
-  name_hash[:name]
   end
 end
